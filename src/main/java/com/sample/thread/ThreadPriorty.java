bf1cd1b56aed8f64d7d2fa99bbc3aaeed7e5c3c8
John_Yang <jiahong9yang@126.com>
2019-03-07 15:21:37 +0800

    多线程基础

---
+++ src/main/java/com/sample/thread/ThreadPriorty.java
@@ -0,0 +1,27 @@
+package com.sample.thread;
+
+public class ThreadPriorty {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(
+                ()->{
+                    while(true){
+                        System.out.println("t1");
+                    }
+                }
+        );
+        t1.setPriority(3);
+
+        Thread t2 = new Thread(
+                () -> {
+                    while(true){
+                        System.out.println("t2");
+                    }
+                }
+        );
+        t2.setPriority(10);
+
+        t1.start();
+        t2.start();
+    }
+}
