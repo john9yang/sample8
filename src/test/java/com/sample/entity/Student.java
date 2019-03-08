5ae563dd17fa912fc181b4b98b84508c2c2f42b7
John_Yang <jiahong9yang@126.com>
2019-03-08 10:32:20 +0800

    多线程基础

---
+++ src/test/java/com/sample/entity/Student.java
@@ -0,0 +1,27 @@
+package com.sample.entity;
+
+public class Student{
+    String name;
+    private Integer score;
+
+    public Student(String name, Integer score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
