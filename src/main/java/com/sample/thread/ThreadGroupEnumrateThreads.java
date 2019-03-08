b917e8b3453fa2327c4e0fa00d3b6fe328176fe4
John_Yang <jiahong9yang@126.com>
2019-03-08 17:41:39 +0800

    多线程基础

---
+++ src/main/java/com/sample/thread/ThreadGroupEnumrateThreads.java
@@ -0,0 +1,32 @@
+package com.sample.thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadGroupEnumrateThreads {
+
+    public static void main(String[] args) throws InterruptedException{
+        ThreadGroup myGroup = new ThreadGroup("MyGroup");
+        Thread thread = new Thread(myGroup,()->{
+            while(true){
+                try{
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        },"MyThread");
+       thread.start();
+
+       TimeUnit.MILLISECONDS.sleep(2);
+
+       ThreadGroup mainGroup = Thread.currentThread().getThreadGroup();
+
+       Thread[] list = new Thread[mainGroup.activeCount()];
+       int recurseSize = mainGroup.enumerate(list);
+       System.out.println(recurseSize);
+
+       recurseSize = mainGroup.enumerate(list,false);
+        System.out.println(recurseSize);
+    }
+}
