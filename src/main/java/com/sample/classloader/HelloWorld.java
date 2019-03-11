dce5f2772eb7974a07dfbf1a50501497effefbe5
John_Yang <jiahong9yang@126.com>
2019-03-11 15:11:06 +0800

    类加载器

---
+++ src/main/java/com/sample/classloader/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sample.classloader;
+
+public class HelloWorld {
+    static
+    {
+        System.out.println("Hello World Class is Initialized.");
+    }
+
+    public String welcome(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/sample/classloader/MyClassLoaderTest.java
@@ -0,0 +1,22 @@
+package com.sample.classloader;
+
+import javax.management.ReflectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MyClassLoaderTest {
+
+    public static void main(String[] args) throws ClassNotFoundException,IllegalAccessException,InstantiationException,
+    NoSuchMethodException, InvocationTargetException {
+        MyClassLoader classLoader = new MyClassLoader();
+        Class<?> aClass = classLoader.loadClass("com.sample.classloader.HelloWorld");
+        System.out.println(aClass.getClassLoader());
+
+        Object helloword = aClass.newInstance();
+        System.out.println(helloword);
+
+        Method welcomeMethod = aClass.getMethod("welcome");
+        String result = (String) welcomeMethod.invoke(helloword);
+        System.out.println("Result:"+result);
+    }
+}
