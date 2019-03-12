566b36d9b30d91093f23c3cb70af0dc936092332
John_Yang <jiahong9yang@126.com>
2019-03-12 11:29:20 +0800

    多线程基础

---
+++ src/main/java/com/sample/thread/designPartern/SingletonByEnum.java
@@ -0,0 +1,32 @@
+package com.sample.thread.designPartern;
+
+public class SingletonByEnum {
+    //实例变量
+    private byte[] data = new byte[1024];
+
+    private SingletonByEnum(){
+
+    }
+
+    //使用枚举充当holder
+    private enum EnumHolder{
+        INSTANCE;
+
+        private SingletonByEnum instance;
+
+        EnumHolder(){
+            System.out.println("EnumHolder will be initialized immediately");
+            this.instance = new SingletonByEnum();
+        }
+
+        private SingletonByEnum getSingleton(){
+            return instance;
+        }
+    }
+
+    public static SingletonByEnum getInstance(){
+        System.out.println("Enter getInstance");
+        return EnumHolder.INSTANCE.getSingleton();
+    }
+
+}
