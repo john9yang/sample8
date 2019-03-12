1ffc2bba821132fc324d8655b839d9027a1a54a2
John_Yang <jiahong9yang@126.com>
2019-03-12 11:14:45 +0800

    多线程基础

---
+++ src/main/java/com/sample/thread/designPartern/SingletonByHolder.java
@@ -0,0 +1,21 @@
+package com.sample.thread.designPartern;
+
+//不允许被继承
+public final class SingletonByHolder {
+
+    private byte[] data = new byte[1024];
+
+    private SingletonByHolder(){
+
+    }
+
+    //在静态内部类中持有singleton的实例,并且可被直接初始化
+    private static class Holder{
+        private static SingletonByHolder instance = new SingletonByHolder();
+    }
+
+    //获取Holder的instance静态属性
+    public static SingletonByHolder getInstance(){
+        return Holder.instance;
+    }
+}
