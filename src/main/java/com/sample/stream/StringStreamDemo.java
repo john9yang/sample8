33cbb09d4d7f341b63b80bfa87c6a0b33605ad7e
John_Yang <jiahong9yang@126.com>
2019-04-29 15:06:20 +0800

    string stream

---
+++ src/main/java/com/sample/stream/StringStreamDemo.java
@@ -0,0 +1,16 @@
+package com.sample.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringStreamDemo {
+
+    public static void main(String[] args) {
+        List<String> myList = Arrays.asList("area","block","building","city","country");
+
+        myList.stream().filter(s -> s.startsWith("c"))
+                       .map(String::toUpperCase)
+                       .sorted()
+                       .forEach(System.out::println);
+    }
+}
