f13b4caba1e0784f400b4286660cb4259d6eca5d
John_Yang <JohnYang1@thomsonreuters.com>
2019-09-16 14:00:55 +0800

    int stream demo

--- src/main/java/com/sample/stream/IntStreamDemo.java
+++ src/main/java/com/sample/stream/IntStreamDemo.java
@@ -3,7 +3,12 @@ package com.sample.stream;
 import java.util.stream.IntStream;
 
 public class IntStreamDemo {
-    public static void main(String[] args) {
-        IntStream.range(0,5).boxed().map(i -> new Thread(() -> System.out.println(Thread.currentThread().getName()))).forEach(Thread::start);
-    }
+  public static void main(String[] args) {
+    IntStream.range(0, 5)
+        .boxed()
+        .map(i -> new Thread(() -> System.out.println(Thread.currentThread().getName())))
+        .forEach(Thread::start);
+
+    System.out.println(IntStream.range(1, 5).reduce(1, (x, y) -> x * y));
+  }
 }
