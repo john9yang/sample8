5b3aa316afb582e20ba7990c2ec485408a42124a
John_Yang <JohnYang1@thomsonreuters.com>
2019-10-24 09:56:02 +0800

    java current hashmap with weak key

--- src/main/java/com/sample/basic/StringTokenizerDemo.java
+++ src/main/java/com/sample/basic/StringTokenizerDemo.java
@@ -3,6 +3,7 @@ package com.sample.basic;
 import java.util.StringTokenizer;
 
 public class StringTokenizerDemo {
+    public static final String hello = "hello";
 
     public static void main(String[] args) {
         String content = "oracle,java,ibm,jdk,spring";
@@ -11,5 +12,8 @@ public class StringTokenizerDemo {
            String token =st.nextToken();
             System.out.println("token:"+token);
         }
+
+        String world="world";
+        System.out.println(hello.concat("_").concat(world));
     }
 }
