423140dfb19a640b305592ee5ae3c8ce011aa8ad
John_Yang <JohnYang1@thomsonreuters.com>
2019-12-25 14:25:39 +0800

    add class init

--- src/main/java/com/sample/classloader/ExtClassLoader.java
+++ src/main/java/com/sample/classloader/ExtClassLoader.java
@@ -3,7 +3,9 @@ package com.sample.classloader;
 public class ExtClassLoader {
     public static void main(String[] args) throws ClassNotFoundException {
         System.out.println(System.getProperty("java.ext.dirs"));
+        System.out.println(System.getProperty("java.class.path"));
         Class<?> simpleClass = Class.forName("com.sample.classloader.Simple");
         System.out.println(simpleClass.getClassLoader());
+        System.out.println(ExtClassLoader.class.getClassLoader());
     }
 }
