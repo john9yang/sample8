a172d1543d112951d31565eba0d05ddc54f71759
john9yang <jiahong9yang@126.com>
2020-07-09 11:06:19 +0800

    java8 interface default method

---
+++ src/main/java/jvm/HeapOOM.java
@@ -0,0 +1,23 @@
+package jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ *
+ * @author John_Yang
+ */
+public class HeapOOM {
+
+    static class OOMObject{
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+
+        while(true){
+            list.add(new OOMObject());
+        }
+    }
+}
